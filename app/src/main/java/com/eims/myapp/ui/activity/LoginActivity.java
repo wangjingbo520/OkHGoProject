5d1191e3a7bb59f5899fa6c29844d68bca7de461
kirk <18120446428@163.com>
2018-09-06 17:16:03 +0800

    增加图片选择库

--- app/src/main/java/com/eims/myapp/ui/activity/LoginActivity.java
+++ app/src/main/java/com/eims/myapp/ui/activity/LoginActivity.java
@@ -7,6 +7,12 @@ import android.view.View;
 import com.eims.myapp.MainActivity;
 import com.eims.myapp.R;
 import com.eims.myapp.base.MyNetDataBaseActivity;
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
+
+import java.util.List;
 
 import butterknife.OnClick;
 
@@ -24,7 +30,7 @@ public class LoginActivity extends MyNetDataBaseActivity {
         setTitle("登录");
     }
 
-    @OnClick(R.id.tvLogin)
+    @OnClick({R.id.tvLogin, R.id.tvPicture})
     @Override
     public void onClick(View view) {
         super.onClick(view);
@@ -32,8 +38,35 @@ public class LoginActivity extends MyNetDataBaseActivity {
             case R.id.tvLogin:
                 startActivity(new Intent(mContext, MainActivity.class));
                 break;
+            case R.id.tvPicture:
+                PictureSelector.create(this)
+                        .openGallery(PictureMimeType.ofImage())
+                        .forResult(PictureConfig.CHOOSE_REQUEST);
+                break;
             default:
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case PictureConfig.CHOOSE_REQUEST:
+                    // 图片、视频、音频选择结果回调
+                    List<LocalMedia> selectList = PictureSelector.obtainMultipleResult(data);
+                    // 例如 LocalMedia 里面返回三种path
+                    // 1.media.getPath(); 为原图path
+                    // 2.media.getCutPath();为裁剪后path，需判断media.isCut();是否为true  注意：音视频除外
+                    // 3.media.getCompressPath();为压缩后path，需判断media.isCompressed();是否为true  注意：音视频除外
+                    // 如果裁剪并压缩了，以取压缩路径为准，因为是先裁剪后压缩的
+//                    adapter.setList(selectList);
+//                    adapter.notifyDataSetChanged();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
